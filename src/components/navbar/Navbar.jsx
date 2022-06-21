93de6cc66cebb3a13c77b18829657760f10df7ba
Cup of Coding <anne@cupofcoding.com>
2022-06-20 23:57:59 -0700

    added basic navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,15 @@ import "./navbar.css"
 
 const Navbar = () => {
   returN (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <div className="navContainer">
+            <span className="logo">PeasTravel</span>
+            <div className="navItems">
+                <button className="navButton">Register</button>
+                <button className="navButton">Login</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
