9932524199505e623bb4261d5b09593fdc128753
Cup of Coding <anne@cupofcoding.com>
2022-06-21 23:48:18 -0700

    added libraries for icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <div className="header">
         <div className="headerList">
             <div className="headerListItem">
-                <FontAwesomeIcon icon={fa-light fa-bed} />
+                <FontAwesomeIcon icon={faBed} />
             </div>
         </div>
     </div>
