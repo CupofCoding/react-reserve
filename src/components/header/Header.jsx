bb0d3e944742b596e9f321a52232c4e85f83f78e
Cup of Coding <anne@cupofcoding.com>
2022-06-21 23:40:24 -0700

    added libraries for icons

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerList">
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={fa-light fa-bed} />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
