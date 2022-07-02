87d83b5128efc9eb71c8979f93042ad74f81d8a8
Cup of Coding <anne@cupofcoding.com>
2022-07-01 23:39:06 -0700

    header icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCar, faFaceGrinBeam, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
@@ -9,6 +9,22 @@ const Header = () => {
             <div className="headerListItem">
                 <FontAwesomeIcon icon={faBed} />
                 <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car Rentals</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faFaceGrinBeam} />
+                <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Taxis</span>
             </div>
         </div>
     </div>
